0ad7e95f3d1c22cd10c9a208b8f2aa4dd10c0d12
Jeremias Gomez Giglio <72324512+JereGomez@users.noreply.github.com>
2024-06-27 22:44:01 -0300

    Update UbicacionController.java

--- src/main/java/com/example/demo/controller/UbicacionController.java
+++ src/main/java/com/example/demo/controller/UbicacionController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("ubicacion")
+@RequestMapping("/ubicacion")
 public class UbicacionController {
 
 

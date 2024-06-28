1055cf605d636eb240dbc1c70ff58cf6ac4d464b
Jeremias Gomez Giglio <72324512+JereGomez@users.noreply.github.com>
2024-06-27 22:43:28 -0300

    Update FavoritoController.java

--- src/main/java/com/example/demo/controller/FavoritoController.java
+++ src/main/java/com/example/demo/controller/FavoritoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("favoritos")
+@RequestMapping("/favoritos")
 public class FavoritoController {
     private final Logger LOGGER = LoggerFactory.getLogger(FavoritoController.class);
 

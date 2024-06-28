194a9167e2c761696d64c4787dd9b22a143399b8
Jeremias Gomez Giglio <72324512+JereGomez@users.noreply.github.com>
2024-06-27 22:43:41 -0300

    Update ProductoController.java

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("productos")
+@RequestMapping("/productos")
 public class ProductoController {
     @Autowired
     public IProductoService productoService;
